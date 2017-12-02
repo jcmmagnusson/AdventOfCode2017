ba5fffe192b1d74bd29cb112aba0fe6c9ddd901d
Johan Magnusson <jcmmagnusson@gmail.com>
2017-12-02 13:58:42 +0100

    Day 2

---
+++ day2.exs
@@ -0,0 +1,45 @@
+defmodule Day2 do
+
+  def calculate_checksum(file_path) do
+    parse_file(file_path)
+    |> Enum.map(&checksum_row/1)
+    |> Enum.sum()
+  end
+
+  def divide_thingey(file_path) do
+    parse_file(file_path)
+    |> Enum.map(&divide_row/1)
+    |> Enum.sum()
+  end
+
+  defp parse_file(file_path) do
+    File.read!(file_path)
+    |> String.split("\n")
+    |> Enum.map(&String.split/1)
+    |> Enum.map(fn numbers -> Enum.map(numbers, &String.to_integer/1) end)
+    |> List.delete_at(-1)
+  end
+
+  defp checksum_row(num_row) do
+    Enum.max(num_row) - Enum.min(num_row)
+  end
+
+  defp divide_row([_ | []]), do: 0
+
+  defp divide_row(num_row) do
+    blabla(num_row) + divide_row(tl(num_row))
+  end
+
+  defp blabla([x | xs]) do
+    xs
+    |> Enum.map(fn num -> divide(x, num) end)
+    |> Enum.sum()
+  end
+
+  defp divide(x, y) when rem(x, y) == 0, do: div(x, y)
+  defp divide(x, y) when rem(y, x) == 0, do: div(y, x)
+  defp divide(_, _),                     do: 0
+end
+
+IO.puts Day2.calculate_checksum("day2.txt") # Part 1
+IO.puts Day2.divide_thingey("day2.txt")     # Part 2
